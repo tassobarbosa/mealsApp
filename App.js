f38443d7682147278d47741736a7eda623f4f64c
Tasso <tassobarbosa@gmail.com>
2021-04-27 07:58:11 -0300

    Começando novo projeto

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default function App(){
+  return(
+    <View>
+      <Text>Começando app</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
